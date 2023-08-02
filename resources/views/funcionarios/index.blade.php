ad7f7e2651a4d767dd944831ca621c9669013ea2
Eduardo Dias <eduardoo.js19@gmail.com>
2023-08-02 20:17:19 -0300

    routes

--- resources/views/funcionarios/index.blade.php
+++ resources/views/funcionarios/index.blade.php
@@ -2,7 +2,10 @@
 @section('title', 'Funcionarios')
 
 @section('content')
-    <x-btn-create/>
+    <x-btn-create>
+        <x-slot name="route">/funcionarios/novo</x-slot>
+        <x-slot name="title">Cadastrar Funcrionário</x-slot>
+    </x-btn-create>
     <h1 class="fs-2 mb-3">Lista Funcionários</h1>
 
     <table class="table table-striped">
